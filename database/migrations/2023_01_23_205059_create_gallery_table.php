445189b1bf863e836ccd06c3081e77f74805f7d4
Your Name <menniablaise@yahoo.com>
2023-01-23 21:09:50 +0000

    modify gallery db

---
+++ database/migrations/2023_01_23_205059_create_gallery_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('gallery', function (Blueprint $table) {
+            $table->renameColumn('image_orientation', 'orientation')->after('image');
+            $table->renameColumn('category', 'album')->after('orientation');
+            $table->dropColumn('image_height');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gallery');
+    }
+};
