ff0f8a0d53fb3be9c9ee173ff4f612cf00f137ce
Your Name <menniablaise@yahoo.com>
2023-05-17 23:01:56 +0000

    i

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -36,7 +36,7 @@ class AdminHomeController extends Controller
             'video_link' => $request->video_link
         ]);
 
-        return back()->with('success', 'Header Uploaded Successfully');
+        return redirect()->back()->with('success', 'Header Uploaded Successfully');
         
     }
 }
