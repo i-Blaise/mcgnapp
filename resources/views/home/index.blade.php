4abc11ee0c6ac89d7ab2317b0abbe022335be76a
Blaise <menniablaise@yahoo.com>
2023-12-30 13:18:50 +0000

    mod page titles

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
         <meta charset="utf-8">
-        <title>MCGN</title>
+        <title>MCGN - Home</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="Free Website Template" name="keywords">
         <meta content="Free Website Template" name="description">
